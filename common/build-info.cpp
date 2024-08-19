ae57cd514fb826ff5c1f34be2a4e0005ef40ba0d
Sachin Desai <sdesai@salesforce.com>
2024-08-19 11:48:05 -0700

    added additional files

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 3611;
+char const *LLAMA_COMMIT = "7669f70e";
+char const *LLAMA_COMPILER = "Apple clang version 15.0.0 (clang-1500.3.9.4)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin23.6.0";
